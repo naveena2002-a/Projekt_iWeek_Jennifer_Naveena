b1f20bad7f30c29a5fcac758c28029015c5b5ee7
naveena2002-a <75990719+naveena2002-a@users.noreply.github.com>
2020-12-16 10:38:30 +0100

    Add files via upload

---
+++ Space_Invaders.py
@@ -0,0 +1,16 @@
+
+
+import pygame
+
+pygame.init()
+
+
+width = 700
+height = 500
+
+win = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Space Invaders")
+run = True
+
+figur1 = Figur()
+
