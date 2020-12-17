0607a2e409b6f2b697078d1dfabd2e9917aa0251
naveena2002-a <75990719+naveena2002-a@users.noreply.github.com>
2020-12-17 08:45:01 +0100

    Update Space_Invaders.py

--- Space_Invaders.py
+++ Space_Invaders.py
@@ -1,18 +1,38 @@
 
 
-import pygame
+import curses
 
-pygame.init()
+s = curses.initscr()
+curses.curs_set(0)
+sh, sw = s.getmaxyx()
+w = curses.newwin(sh, sw, 0, 0)
+w.keypad(1)
+w.timeout(100)
+snake_x = sw/4
+snake_y = sh/2f
+snake = [
+    [snake_y, snake_x],
+    [snake_y, snake_x-1],
+    [snake_y, snake_x-2]
+]
+food = [sh/2, sw/2]
+w.addch(food[0], food[1], curses.ACS_PI)
+
+while True:
+    next_key = w.getch()
+    key = key if next_key == -1 else next_key
+
+    new_head = [snake[0][0], snake[0][1]]
+    if key == curses.KEY.DOWN:
+        new_head[0] += 1
+    if key == curses.KEY_UP:
+        new_head[0] -= 1
+    if key == curses.KEY.LEFT:
+        new_head[1] -= 1
+    if key == curses.KEY_RIGHT:
+        new_head[1] += 1
 
 
-width = 700
-height = 500
-
-win = pygame.display.set_mode((width, height))
-pygame.display.set_caption("Space Invaders")
-run = True
 
 
 
-
-s
